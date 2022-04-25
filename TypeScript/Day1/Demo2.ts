419bd1e4e56a1f68763057d61fb976b39d0c5024
Madhavi-Yadav <104131831+Madhavi-Yadav@users.noreply.github.com>
2022-04-25 14:10:52 +0530

    Create Demo2.ts

---
+++ TypeScript/Day1/Demo2.ts
@@ -0,0 +1,18 @@
+var empID:number  = 5464654;
+var Name:string  = "Scott";
+var Job:string  = "Manager";
+var Salary:number  = 45000;
+var DeptNo:number  = 10;
+var isPermanentEmployee:boolean = true;
+
+console.log("Employee ID  :" + empID);
+console.log("Employee Name  :" + Name);
+console.log("Employee Job  :" + Job);
+console.log("Is Permanent Employee :" + isPermanentEmployee);
+console.log("Employee Salary : INR " + Salary);
+console.log("Employee DeptNo : " + DeptNo);
+
+console.log("------------------------------------------------");
+
+var strOutput = `Employee Details ID: ${empID}, Name : ${Name}, Job : ${Job}, Is Permanent Employee : ${isPermanentEmployee}, Salry : INR ${Salary}, DeptN: ${DeptNo}`; 
+console.log(strOutput);
