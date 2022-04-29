4c04c214a8a4faa943c5470db0ab4274f8436212
Madhavi-Yadav <104131831+Madhavi-Yadav@users.noreply.github.com>
2022-04-29 17:05:32 +0530

    Update filter-range.pipe.ts

--- Angular/Day3/Custom Pipe with paramters/filter-range.pipe.ts
+++ Angular/Day3/Custom Pipe with paramters/filter-range.pipe.ts
@@ -9,7 +9,7 @@ export class FilterRangePipe implements PipeTransform {
    transform(inputData:any[], min:number, max:number): any[]
   {
     if(min !=0 && max !==0){
-    return inputData.filter(item => item.salary <= max && item.salary >= min);
+     return inputData.filter(item => item["Salary"] >= key && item['Salary'] <= value);
     }else {
       return inputData;
     }
