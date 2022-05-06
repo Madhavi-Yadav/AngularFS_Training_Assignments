bee853af1d9f475944dd8076f7e7363607e4d210
Madhavi-Yadav <104131831+Madhavi-Yadav@users.noreply.github.com>
2022-05-06 14:48:33 +0530

    Add files via upload

---
+++ Angular/Day7/Model-Forms/model-forms/model-forms.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-model-forms',
+  templateUrl: './model-forms.component.html',
+  styleUrls: ['./model-forms.component.css']
+})
+export class ModelFormsComponent implements OnInit {
+
+  RegForm:FormGroup = new FormGroup({
+    oname : new FormControl("", Validators.required),
+    cnumber : new FormControl("", [Validators.required,Validators.pattern("\\d{10}")]),
+    vregno : new FormControl("", [Validators.required, Validators.pattern("[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}")]),
+    email : new FormControl("", [Validators.required,Validators.email]),
+    address : new FormControl("", Validators.required),
+    model:new FormControl("", Validators.pattern("[a-z]{1,10}")),
+    color:new FormControl("", Validators.required),
+    year:new FormControl("", Validators.pattern("\\b(201[2-9]|2020|202[1-2])\\b")),
+  });
+
+ constructor() { }
+
+ ngOnInit(): void {
+ }
+
+
+
+ public submit_click():void
+ {
+     // ajax call to send data to server
+     alert("Vehical Registration is Successful.");
+     let VehicleObj = this.RegForm.value;
+     console.log(VehicleObj);
+ }
+
+}
